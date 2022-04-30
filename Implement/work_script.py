2b4602b74a237fdb22887d7dacc5fb7e56d3b4ed
tananett <nothinglive1@hotmail.com>
2022-04-30 23:18:20 +0700

    add py script

--- Implement/work_script.py
+++ Implement/work_script.py
@@ -1,4 +1,5 @@
 import datetime
+import pytz
 import requests
 import pandas as pd
 import joblib
@@ -29,6 +30,18 @@ def map_traffic_with_latlon(df):
                          if len(df_km127[(df_km127['rd'] == row['road_number']) & (df_km127['km'] == row['km'])]['lon'].values) > 0 else 0, axis=1)
     return df
 
+def lineNotify(post_data):
+    headers = {
+        'content-type':
+        'application/x-www-form-urlencoded',
+        'Authorization': 'Bearer ' + 'Rr0wbQYQhq07gzE0knhSSbFNFAS8UtCfoFNX5H5uBSm'
+    }
+    msg = f'{post_data[10]} Anomaly at \nRoad number : {post_data[0]} \nKm : {post_data[1]} \nDirection : {post_data[2]}\nLatitude : {post_data[8]} \nLongitude : {post_data[9]} \n'
+    glink = f'https://www.google.com/maps/dir//{post_data[8]}+,+{post_data[9]}'
+    send_msg = f'{msg}{glink}'
+    r = requests.post('https://notify-api.line.me/api/notify',
+                      headers=headers, data={'message': send_msg})
+
 def job():
     now = datetime.datetime.now()
     print('***REQUESTING NEW DATA********************************')
@@ -55,7 +68,7 @@ def job():
             road_data = map_traffic_with_latlon(road_data)
             current_data = road_data[(road_data['road_number']==road) & (road_data['direction']==direction)]
             current_data['portion_speed'] = current_data['avg_speed']/current_data['mffs']
-            
+
             x_predict = current_data[num_cols]
             y_predict = gnb_model.predict(x_predict)
             current_data['cluster'] = y_predict
@@ -65,20 +78,37 @@ def job():
             for i in y_predict_proba :
                 problist.append(round(i[sus_cluster],3))
             current_data['prob'] = problist
-            current_data = current_data[(current_data['cluster']==sus_cluster) & (current_data['prob']>=0.8) & (current_data['all_units']>=1)]
-            post_data = current_data[['road_number','km','direction','inflow_units','outflow_unit','samecell_units','all_units','avg_speed','lat','lon','datetime']]
-            post_data = post_data.values.tolist()
+            current_data = current_data[(current_data['cluster']==sus_cluster) & (current_data['prob']>=0.8) & (current_data['all_units']>=1) & (current_data['avg_speed']>0)]
+            if current_data.empty :
+                post_data = []
+            else :              
+                post_data = current_data[['road_number','km','direction','inflow_units','outflow_unit','samecell_units','all_units','avg_speed','lat','lon','datetime']]
+                # post_data['datetime'] = post_data['datetime'].dt.tz_localize('Etc/GMT+7').dt.tz_convert('America/New_York')
+                # print(post_data.dtypes)
+                post_data = post_data.values.tolist()
+
+            
+
+            
+
+            
 
             url = 'https://tad-api-v1.herokuapp.com/api/accident/bulk'
 
             print(f'Prediction of road {road} : {direction} : DONE ...')
             if len(post_data) != 0 :
+                print(post_data[0][-1])
+
                 result = requests.post(url, json=post_data)
                 
                 print(f'Posting to Backend {len(post_data)} items')
                 print(f'Status code : {result.status_code} , reason : {result.reason}')
             else :
                 print(f'Not Detect anomaly, skipping ....')
+
+            # if(post_data) :
+            #     for i in post_data :
+            #         lineNotify(i)
 
             print('------------------------------------')
 
